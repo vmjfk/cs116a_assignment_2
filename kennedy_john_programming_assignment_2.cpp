d4fdfc87d9ec96ccc8a46e339d8b43fb7e2b0952
vmjfk <vmjfk@kennedylaws.org>
2017-09-19 13:59:24 -0700

    still  editing prog 2

--- kennedy_john_programming_assignment_2.cpp
+++ kennedy_john_programming_assignment_2.cpp
@@ -7,20 +7,56 @@
 int main (int argc, char *argv[])
 {  
 	glutInit (&argc, argv);
-	glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
+	glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH | GLUT_ALPHA);
 	glutInitWindowSize (X_RESOLUTION, Y_RESOLUTION);
 	glutInitWindowPosition (0,0);
-	glutCreateWindow ("Mouse keyboard click demo");
+	glutCreateWindow ("Flying camera");
 	glutDisplayFunc (display);
 	glutReshapeFunc (reshape);
-	glutMouseFunc (mouse);
+	
 	glutKeyboardFunc (keyboard);
 	glutSpecialFunc (arrow_keys);
+
+    /* instantiate other objects here
+    */
+
 	glutMainLoop ();
 }
-void keyboard (unsigned char key, int x, int y)
+void arrow_keys (unsigned char key, int x, int y)
 {
     switch (key)
     {
-        case 
+        case GLUT_KEY_LEFT:
+            break;
+        case GLUT_KEY_RIGHT:
+            break;
+        case GLUT_KEY_UP:
+            break; 
+        case GLUT_KEY_DOWN:
+            break;
+        default:
+            break;
+    }
+
 }
+void Init()
+{
+    glShadeModel (GL_SMOOTH);
+    glEnable (GL_DEPTH_TEST);
+
+}
+void keyboard (unsigned char key, int x, int y)
+{
+
+}
+
+void display()
+{
+
+}
+
+void reshape() 
+{
+
+}
+void 
